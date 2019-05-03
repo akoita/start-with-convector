b4dc86bc74729285416758dfa947ae461e370ab4
akoita@vagrant@ubuntu-xenial <aboubakar.koita@yahoo.fr>
2019-05-03 13:53:18 +0200

    Project init

---
+++ packages/participant-cc/src/participant.controller.ts
@@ -0,0 +1,20 @@
+import { ChaincodeTx } from '@worldsibu/convector-platform-fabric';
+import {
+  Controller,
+  ConvectorController,
+  Invokable,
+  Param
+} from '@worldsibu/convector-core';
+
+import { Participant } from './participant.model';
+
+@Controller('participant')
+export class ParticipantController extends ConvectorController<ChaincodeTx> {
+  @Invokable()
+  public async create(
+    @Param(Participant)
+    participant: Participant
+  ) {
+    await participant.save();
+  }
+}
